39f9f880e867c03641f293a0e8bf425f2413282b
heberGustavo <heber.gbarbosa@hotmail.com>
2024-08-13 08:12:19 -0300

    Update componento to send params

---
+++ execicios/src/pages/basicos/usandoTitulo.jsx
@@ -0,0 +1,9 @@
+import Titulo from "../../../components/Titulo";
+
+export default function usandoTitulo(){
+    return(
+        <>
+            <Titulo title="Teste" subtitle="subtitulo"/>
+        </>
+    );
+}
