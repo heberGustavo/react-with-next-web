8ff627e100da6252825a5583ccc993a521a1edb9
heberGustavo <heber.gbarbosa@hotmail.com>
2024-08-27 09:53:05 -0300

    Modify mode to view data

--- challenges/mega-sena/components/Container/Container.jsx
+++ challenges/mega-sena/components/Container/Container.jsx
@@ -1,16 +1,29 @@
 import styles from './Container.module.css'
 
+import { useEffect, useState } from 'react';
+import { mega } from "../../functions/mega";
+
 import Titulo from "../Titulo/Titulo";
 import ContainerNumeros from "../ContainerNumeros/ContainerNumeros";
-import GeradorNumeros from '../GeradorNumeros/GeradorNumeros';
 
 export default function Container(){
+
+    const [numeros, setNumeros] = useState([]);
+
+    useEffect(() => {
+        setNumeros(mega())
+    }, [])
+
     return(
         <div className={styles.container}>
             <Titulo titulo="MEGA SENA" subtitulo="Gere aqui seus números da Sorte!" />
-            <br />
+            <ContainerNumeros numeros={numeros} />
 
-            <GeradorNumeros quantidadeNumeros={6} />
+            <br />
+            <button 
+                className={styles.button}
+                onClick={() => setNumeros(mega())}
+            >Gerar</button>
         </div>
     );
 }
