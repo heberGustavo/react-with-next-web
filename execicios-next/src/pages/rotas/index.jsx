f6867c2a763a8ec8a8f4e7d0053cc6d2d4c40060
heberGustavo <heber.gbarbosa@hotmail.com>
2024-08-28 14:41:34 -0300

    Navigation with Router in Next

--- execicios-next/src/pages/rotas/index.jsx
+++ execicios-next/src/pages/rotas/index.jsx
@@ -1,20 +1,44 @@
 import Link from "next/link";
+import router from "next/router";
 
 export default function rotas(){
+    
+    const UrlBuscar = "/rotas/123/buscar";
+    const UrlParams = "/rotas/params?nomeCompleto=HeberGustavo&apelido=Gustavo";
+    const UrlIdWithName = "/rotas/321/HEBER";
+
+    const navegacaoSimples = (url) => {
+        router.push(url);
+    }
+
+    const navegacaoComParams = () => {
+        router.push({
+            pathname: "/rotas/params",
+            query: {
+                id: 123,
+                nome: "Navegação Fixa"
+            }
+        });
+    }
     return(
         <div>
             <h1>Rotas</h1>
             <ul>
-                <Link href="/rotas/params?nomeCompleto=HeberGustavo&apelido=Gustavo">
+                <Link href={UrlParams}>
                     <li>Redirect to Params</li>
                 </Link>
-                <Link href="/rotas/123/buscar">
+                <Link href={UrlBuscar}>
                     <li>Redirect with ID</li>
                 </Link>
-                <Link href="/rotas/123/HEBER">
+                <Link href={UrlIdWithName}>
                     <li>Redirect with ID and NAME</li>
                 </Link>
             </ul>
+            <div style={{display: "flex", flexDirection: "column", alignItems: "flex-start"}}>
+                <button onClick={navegacaoComParams}>Params</button>
+                <button onClick={() => router.push(UrlBuscar)}>Buscar</button>
+                <button onClick={() => navegacaoSimples(UrlIdWithName)}>ID with Name</button>
+            </div>
         </div>
     );
 }
