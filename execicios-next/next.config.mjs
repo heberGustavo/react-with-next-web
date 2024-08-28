1c60b661baff4875e702d4baf33224d0a3c1ee68
heberGustavo <heber.gbarbosa@hotmail.com>
2024-08-28 08:05:44 -0300

    example path do DB

--- execicios-next/next.config.mjs
+++ execicios-next/next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    //{process.env.dbPath}
+    dbPath: 'mysql...'
+  }
 };
 
 export default nextConfig;
