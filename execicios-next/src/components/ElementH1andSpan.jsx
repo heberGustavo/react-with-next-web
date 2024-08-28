dfea4a59e329d6bf18ddde463c9d5becf8849846
heberGustavo <heber.gbarbosa@hotmail.com>
2024-08-28 09:24:15 -0300

    Dynamic routes and elements

---
+++ execicios-next/src/components/ElementH1andSpan.jsx
@@ -0,0 +1,8 @@
+export default function ElementH1andSpan(props){
+    return(
+        <div>
+            <span><strong>{props.e1}: </strong></span>
+            <span>{props.e2}</span>
+        </div>
+    );
+}
---
+++ execicios-next/src/pages/rotas/params.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import { useRouter } from "next/router";
+import ElementH1andSpan from "../../components/ElementH1andSpan";
+
+export default function params(){
+    return(
+        <div>
+            <h1>Params</h1>
+            <h3>This page get info in URL dynamic</h3>
+            <span>Example: [...]params?nome=heberGustavo&idade=25&peso=80</span>
+            <br /><br />
+            <div>
+                {getElementsInQuery()}
+            </div>
+        </div>
+    );
+}
+
+const getElementsInQuery = () => {
+    const params = useRouter().query;
+
+    let arrayItems = Object.entries(params);
+    return arrayItems.map((element, i) => <ElementH1andSpan key={i} e1={element[0]} e2={element[1]}  />)
+}
