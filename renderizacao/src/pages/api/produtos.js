91809069ac5f8b7cb77036b119d78a444a92f263
heberGustavo <heber.gbarbosa@hotmail.com>
2024-09-06 07:30:23 -0300

    Create component with ServerSideProps

--- renderizacao/src/pages/api/produtos.js
+++ renderizacao/src/pages/api/produtos.js
@@ -1,9 +1,13 @@
+function numeroAleatorio(min = 1, max = 10000){
+  return parseInt(Math.random() * (max - min)) + min;
+}
+
 export default function handler(req, res) {
   res.status(200).json([
-    { id: 1, nome: "Caneta", preco: 5.60 },
-    { id: 2, nome: "Borracha", preco: 1.20 },
-    { id: 3, nome: "Caderno", preco: 15.30 },
-    { id: 4, nome: "Tesoura", preco: 3.60 },
+    { id: numeroAleatorio(), nome: "Caneta", preco: 5.60 },
+    { id: numeroAleatorio(), nome: "Borracha", preco: 1.20 },
+    { id: numeroAleatorio(), nome: "Caderno", preco: 15.30 },
+    { id: numeroAleatorio(), nome: "Tesoura", preco: 3.60 },
   ]
   );
 }
